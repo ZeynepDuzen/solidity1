29001a5b0bd107c506a17b24a283da06aceb5116
Zeynep Düzen <99576262+ZeynepDuzen@users.noreply.github.com>
2023-05-11 12:00:11 +0300

    Create solidity1.sol
    writing "Hello World" with solidity

---
+++ solidity1.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+
+//solidity compiler verionu 0.8.17' ye eşit veya büyük olmalıdır
+//SPDX License --> Software Package Data Exchange
+
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+   string public hello = "Hello World!-Add";
+}
